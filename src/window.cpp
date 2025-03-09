62fb8df99c1870d94964f37b3547946eee84fdd0
Daniil Smirnov <daniil.smirnov007@gmail.com>
2025-03-09 20:59:50 +0300

    Initial commit

---
+++ src/window.cpp
@@ -0,0 +1,103 @@
+#include "window.h"
+#include <basetsd.h>
+#include <winuser.h>
+
+Window::WindowConfig Window::WindowConfig::windowConfigSingleton;
+
+Window::WindowConfig::WindowConfig() noexcept
+    : hInst_(GetModuleHandle(nullptr)) {
+    WNDCLASSEX wc {};
+    wc.cbSize = sizeof(wc);
+    wc.style = CS_OWNDC;
+    wc.lpfnWndProc = handleMessageSetup;
+    wc.cbClsExtra = 0;
+    wc.cbWndExtra = 0;
+    wc.hInstance = getInstance();
+    wc.lpszClassName = getName();
+    wc.hbrBackground = nullptr;
+
+    if (!RegisterClassEx(&wc)) {
+        MessageBox(nullptr, "Window registration failed", "Error", MB_ICONEXCLAMATION | MB_OK);
+        return;
+    };
+}
+
+Window::WindowConfig::~WindowConfig() {
+    UnregisterClass(windowClassName.data(), getInstance());
+}
+
+const char *Window::WindowConfig::getName() noexcept {return windowClassName.data();}
+
+HINSTANCE Window::WindowConfig::getInstance() noexcept { return windowConfigSingleton.hInst_; }
+
+Window::Window(int width, int height, const char *name) noexcept {
+    RECT wr {};
+    wr.left = 100;
+    wr.right = width + wr.left;
+    wr.top = 100;
+    wr.bottom = height + wr.left;
+    AdjustWindowRect(&wr, WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU, false);
+
+    HWND hWnd_ {CreateWindow(
+        WindowConfig::getName(), name,
+        WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU,
+        CW_USEDEFAULT, CW_USEDEFAULT, wr.right - wr.left, wr.bottom - wr.top,
+        nullptr, nullptr, WindowConfig::getInstance(), this 
+    )};
+
+    ShowWindow(hWnd_, SW_SHOWDEFAULT);    
+}
+
+Window::~Window() {
+    DestroyWindow(hWnd_);
+}
+
+LRESULT WINAPI Window::handleMessageSetup(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+    if (msg == WM_NCCREATE) {
+        const CREATESTRUCTW *const create {reinterpret_cast<CREATESTRUCTW *>(lParam)};
+        Window *const window {static_cast<class Window *>(create->lpCreateParams)};
+        SetWindowLongPtr(hWnd, GWLP_USERDATA, reinterpret_cast<LONG_PTR>(window));
+        SetWindowLongPtr(hWnd, GWLP_WNDPROC, reinterpret_cast<LONG_PTR>(&Window::handleMessageThunk));
+        return window->handleMessage(hWnd, msg, wParam, lParam);
+    }
+    return DefWindowProc(hWnd, msg, wParam, lParam);
+}
+
+LRESULT CALLBACK Window::handleMessageThunk(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+    auto pWnd {reinterpret_cast<Window*>(GetWindowLongPtr(hWnd, GWLP_USERDATA))};
+    return pWnd->handleMessage(hWnd, msg, wParam, lParam);
+}
+
+
+LRESULT handleMessage(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) {
+    switch (msg) {
+        case WM_CLOSE: {
+        PostQuitMessage(69);
+        break;
+        }
+        case WM_KEYDOWN: {
+        if (wParam == 'F') {
+            SetWindowText(hWnd, "Respects");
+            break;
+        }
+        case WM_KEYUP: {
+        if (wParam == 'F') {
+            SetWindowText(hWnd, "DangerField");
+            SetWindowText(hWnd, char_title.c_str());
+            break;
+        }
+        }
+        case WM_CHAR: {
+            char_title.push_back((char)wParam);
+            SetWindowText(hWnd, char_title.c_str());
+            break;
+        }
+        case WM_LBUTTONDOWN: {
+            POINTS points = MAKEPOINTS(lParam);
+            std::ostringstream oss {};
+            oss <<  "(" << points.x << "," << points.y << ")";
+            SetWindowText(hWnd, oss.str().c_str());
+        }
+
+    }
+}
---
+++ src/window.h
@@ -0,0 +1,34 @@
+#pragma once
+#include <Windows.h>
+#include <minwindef.h>
+#include <string_view>
+
+class Window {
+
+private:
+    class WindowConfig {
+    public:
+        static const char *getName() noexcept;
+        static HINSTANCE getInstance() noexcept;
+    private:
+        WindowConfig() noexcept;
+        ~WindowConfig();
+        WindowConfig(const WindowConfig&) = delete;
+        static constexpr std::string_view windowClassName {"Direct 3D engine window"};
+        static WindowConfig windowConfigSingleton;
+        HINSTANCE hInst_;
+    };
+public:
+    Window(int width, int height, const char* name) noexcept;
+    ~Window();
+    Window(const Window &) = delete;
+    Window & operator=(const Window &) = delete;
+private:
+    static LRESULT CALLBACK handleMessageSetup(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+    static LRESULT CALLBACK handleMessageThunk(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+    LRESULT handleMessage(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
+    int width_;
+    int height_;
+    HWND hWnd_;
+};
