185040c662a94a80ee602346130e942a10d6586f
Daniil Smirnov <daniil.smirnov007@gmail.com>
2025-05-30 17:57:34 +0330

    Add graphics macros

---
+++ src/graphics/exceptions/graphicsExMacros.h
@@ -0,0 +1,20 @@
+#pragma once
+
+// hResult variable should be in scope if you want to use theese
+ 
+#define GFX_EXCEPT_NOINFO(hr) HResultException( __LINE__,__FILE__,hr )
+#define GFX_THROW_NOINFO(hrCall) if (FAILED(hrCall)) throw HResultException( __LINE__,__FILE__,hr )
+#define GFX_THROW_REMOVED_EXCEPT(hr) HResultException( __LINE__,__FILE__,hr )
+
+#ifndef NDEBUG
+#define GFX_EXCEPT(hr) HResultException( __LINE__,__FILE__,(hr),infoManager_.getLog() )
+#define GFX_THROW_INFO(hrcall) infoManager_.set(); if( FAILED( hr = (hrcall) ) ) throw GFX_EXCEPT(hr)
+#define GFX_DEVICE_REMOVED_EXCEPT(hr) DeviceRemovedException( __LINE__,__FILE__,(hr),infoManager_.getLog() )
+#define GFX_THROW_INFO_ONLY(call) infoManager_.set(); (call); {auto v = infoManager_.getLog(); if (!v.empty()) {throw InfoException(__LINE__, __FILE__, v);}}
+#else
+#define GFX_EXCEPT(hr) HResultException( __LINE__,__FILE__,(hr) )
+#define GFX_THROW_INFO(hrcall) GFX_THROW_NOINFO(hrcall)
+#define GFX_DEVICE_REMOVED_EXCEPT(hr) Graphics::DeviceRemovedException( __LINE__,__FILE__,(hr) )
+#define GFX_THROW_INFO_ONLY(call) (call)
+#endif
+
--- src/graphics/graphics.cpp
+++ src/graphics/graphics.cpp
@@ -8,26 +8,10 @@
 #include <d3dcompiler.h>
 #include <array>
 #include <DirectXMath.h>
-
-#define GFX_EXCEPT_NOINFO(hr) HResultException( __LINE__,__FILE__,hr )
-#define GFX_THROW_NOINFO(hrCall) if (FAILED(hrCall)) throw HResultException( __LINE__,__FILE__,hr )
-#define GFX_THROW_REMOVED_EXCEPT(hr) HResultException( __LINE__,__FILE__,hr )
+#include "exceptions/graphicsExMacros.h"
 
 #pragma comment(lib, "d3d11.lib")
 #pragma comment(lib, "d3dcompiler.lib")
-
-
-#ifndef NDEBUG
-#define GFX_EXCEPT(hr) HResultException( __LINE__,__FILE__,(hr),infoManager_.getLog() )
-#define GFX_THROW_INFO(hrcall) infoManager_.set(); if( FAILED( hr = (hrcall) ) ) throw GFX_EXCEPT(hr)
-#define GFX_DEVICE_REMOVED_EXCEPT(hr) DeviceRemovedException( __LINE__,__FILE__,(hr),infoManager_.getLog() )
-#define GFX_THROW_INFO_ONLY(call) infoManager_.set(); (call); {auto v = infoManager_.getLog(); if (!v.empty()) {throw InfoException(__LINE__, __FILE__, v);}}
-#else
-#define GFX_EXCEPT(hr) HResultException( __LINE__,__FILE__,(hr) )
-#define GFX_THROW_INFO(hrcall) GFX_THROW_NOINFO(hrcall)
-#define GFX_DEVICE_REMOVED_EXCEPT(hr) Graphics::DeviceRemovedException( __LINE__,__FILE__,(hr) )
-#define GFX_THROW_INFO_ONLY(call) (call)
-#endif
 
 namespace mw = Microsoft::WRL;
 namespace dx = DirectX;
